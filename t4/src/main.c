28bc37008d2589478a99f065e28b1db0552e704e
Jie Meng <jmengxy@gmail.com>
2020-10-11 21:06:26 +0800

    t4 support link project generated library

--- t4/src/main.c
+++ t4/src/main.c
@@ -1,11 +1,13 @@
 //main.c
 
 #include <hello.h>
+#include "sub/sub.h"
 
 int main()
 {
 
     helloFunc();
+    sub();
 
     return 0;
 }
---
+++ t4/src/sub/sub.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "sub.h"
+
+void sub()
+{
+    printf("sub lib\n");
+}
